6a6649edaf4f407bcce66a4dae61bbdc02906820
johans3p <johan.rochard@S3pweb.com>
2024-09-13 09:57:36 +0200

    feat(plugin): add netbox-plugin-dns

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,10 +4,9 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = [
+    'netbox_dns'
+]
 
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+PLUGINS_CONFIG = {
+}
